216eef104e4b2ea1e7bbc431f9806d2bb0e3f895
amber1o <100294813+amber1o@users.noreply.github.com>
2022-07-29 12:48:10 -0400

    Update namedecoder.py

--- namedecoder.py
+++ namedecoder.py
@@ -59,7 +59,7 @@ class Solution:
                     mode += 1 
                 else:
                     pass
-        final_string = "first name = {0}, last name = {1}, id = {2}".format(f_name,l_name,num)
+        final_string = "First name = {0}, Last name = {1}, id = {2}".format(f_name,l_name,num)
         return final_string
 
                 
